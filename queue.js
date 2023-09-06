ec8bed316fade9f1d8591a5725c5156258261fbc
Tangential Development <139633613+tangentialdev@users.noreply.github.com>
2023-09-06 18:25:43 -0500

    update queue.js

--- queue.js
+++ queue.js
@@ -1,34 +1,39 @@
 class queue {
-    #queue; #stagingQueue;
-    constructor(){
-        this.#queue = [];
-        this.#stagingQueue = [];
-        this.#maxThreads = 50;
+  #queue;
+  #stagingQueue;
+  constructor() {
+    this.#queue = [];
+    this.#stagingQueue = [];
+    this.#maxThreads = 50;
+  }
+  get maxThreads() {
+    //-> int
+    return this.#maxThreads;
+  }
+  set maxThreads(value) {
+    this.#maxThreads = value;
+  }
+  add(func /*function*/) {
+    //-> void
+    this.#queue.length < this.#maxThreads
+      ? this.#queue.push(func)
+      : this.#stagingQueue.push(func);
+  }
+  start() {
+    this.#queue[0]().then(() => {
+      this.#queue.unshift();
+      this.#cycle();
+    });
+  }
+  #cycle() {
+    for (i = this.#queue.length - 1; i < this.#maxThreads; i++) {
+      this.#queue.push(this.#stagingQueue.unshift());
+      let p = this.#queue[i]().then(() => {
+        this.#queue.indexof(p).remove();
+        this.#queue.length > 0 || this.#stagingQueque.length > 0
+          ? this.#cycle()
+          : "";
+      });
     }
-    get maxThreads(){ //-> int
-        return this.#maxThreads;
-    }
-    set maxThreads(value){
-        this.#maxThreads = value;
-    }
-    add(func /*function*/){//-> void
-        (this.#queue.length < this.#maxThreads) ? this.#queue.push(func): this.#stagingQueue.push(func);
-    }
-    start(){
-        this.#queue[0]().then(()=>{
-            this.#queue.unshift();
-            this.#cycle();
-        })
-    }
-    #cycle(){
-        for (i = this.#queue.length - 1; i < this.#maxThreads; i++) {
-            this.#queue.push(this.#stagingQueue.unshift());
-          let p =  this.#queue[i]().then(()=>{
-              this.#queue.indexof(p).remove();
-              (this.#queue.length >0 || this.#stagingQueque.length >0) ? this.#cycle(): '';
-            })
-        }
-        
-    }
-
+  }
 }
