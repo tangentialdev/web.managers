acac008ed5e5c7a9d51350bf1126be7604bb56fe
Tangential Development <139633613+tangentialdev@users.noreply.github.com>
2023-11-09 22:11:26 -0600

    update memory.js

--- memory.js
+++ memory.js
@@ -4,8 +4,8 @@ class xLocalStorage {
   #workingObj;
   constructor(key, obj) {
     this.#key = key;
-    this.#obj = JSON.stringify(obj);
-    localStorage.setItem(this.#key, this.#obj);
+    this.#baseObj = JSON.stringify(obj);
+    localStorage.setItem(this.#key, this.#baseObj);
   }
   get workingObj() {
     this.#refresh();
@@ -17,6 +17,9 @@ class xLocalStorage {
   }
   reset() {
     localStorage.setItem(this.#key, this.#obj);
+  }
+  push(){
+    localStorage.setItem(this.#key, this.#workingObj);
   }
   #refresh() {
     this.#workingObj = JSON.parse(localStorage.getItem(this.#key));
