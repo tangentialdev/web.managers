7799d236f379b979e54ae598d6b74144984608e3
tangentialdev <tangentialdevelopment@gmail.com>
2023-11-11 10:12:20 -0600

    Changes

--- configuration.js
+++ configuration.js
@@ -1,6 +1,7 @@
 //Intentionally JSON formated as string -- On server housing change this to a full JSON file by removing the variable declaration
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let configuration = {
-    "UI_LOGGING_ENABLED": true,
-    "QUEUE_LOGGING_ENABLED": true,
-    "ERROR_LOG_DOCUMENT_ID": 'offcanvas-error-log',
-}
+  UI_LOGGING_ENABLED: true,
+  QUEUE_LOGGING_ENABLED: true,
+  ERROR_LOG_DOCUMENT_ID: "offcanvas-error-log",
+};
