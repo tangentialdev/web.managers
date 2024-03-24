68bf31ca08dad35999ca267b3505d3827269c9af
Tangential Development <139633613+tangentialdev@users.noreply.github.com>
2024-03-24 13:19:40 -0500

    update Node/configuration.js

---
+++ Node/configuration.js
@@ -0,0 +1,7 @@
+//Intentionally JSON formated as string -- On server housing change this to a full JSON file by removing the variable declaration
+let xConfiguration = {
+    "UI_LOGGING_ENABLED": true,
+    "QUEUE_LOGGING_ENABLED": true,
+    "ERROR_LOG_DOCUMENT_ID": 'offcanvas-error-log',
+}
+exports.xConfiguration = xConfiguration;
